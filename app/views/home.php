50d2d32babb6d2948aa207701d6b9643361c18af
Qasim Hussain <compoxition@gmail.com>
2020-04-12 22:29:11 +0500

    m

--- app/views/home.php
+++ app/views/home.php
@@ -1,5 +1,18 @@
 <div class="hero">
-<main>
- <input type="text" class="search" />
-</main>
+<section>
+ <h2>Discover what's near you</h2>
+ <input type="text" class="search" placeholder="Find by name..."/>
+</section>
 </div>
+
+<section class="categories">
+
+<?php for ($i = 1; $i <= 14; $i++) { ?>
+<a href="saloon">
+<img src="<?php echo base_url(); ?>assets/img/category.jpg" alt="category" />
+<label>Category Name</label>
+</a>
+
+<?php } ?>
+
+</section>
