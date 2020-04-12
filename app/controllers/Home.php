85efbb48097de510ca12b6352e641ee44fefc75f
Qasim Hussain <compoxition@gmail.com>
2020-04-12 20:52:41 +0500

    m

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -20,7 +20,9 @@ class Home extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$this->load->view('header');
+		$this->load->view('home');
+		$this->load->view('footer');
 	}
 
     public function signup()
